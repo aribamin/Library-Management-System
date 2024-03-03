12b3498f4ccd6bff04ba5cb4a24161205f6b7f19
Dricmoy <dricbhattacharjee@gmail.com>
2024-03-03 16:27:22 -0700

    Implementing DB_PATH

--- main.py
+++ main.py
@@ -1,56 +1,55 @@
 import sqlite3
+import os
 
-# GLOBAL VARIABLES
-DB_PATH = '' # figure out later
+DB_PATH = os.environ.get('DB_NAME')
 
-conn = sqlite3.connect(DB_PATH)
-c = conn.cursor()
+if DB_PATH is None:
+    print("Database name not set. Please set the DB_NAME environment variable.")
+    print("Do $DB_NAME = ('Enter your database name here, add .db at the end')")
+else:
+    conn = sqlite3.connect(DB_PATH)
+    c = conn.cursor()
 
+    # Could be useful to run queries easier in the program
+    '''
+    A function to execute a query giving a query and set of parameters and return
+    the results as a list of tuples.
+        query: a string which declares the query to be run
+        parameters: a dict which maps the variables declared in the query string
+        to the python variables intended
 
+    Example: simple login query
+    exampleQuery = 'SELECT * FROM members WHERE email=:loginEmail AND passwd=:loginPasswd'
+    exampleParameters = {'loginEmail': emailVar, 'loginPasswd': passwdVar}
+    executeQuery(exampleQuery, exampleParameters)
+    '''
+    def executeQuery(query: str, parameters: dict) -> list:
+        c.execute(query, parameters)
+        rows = c.fetchall()
+        return rows
 
-# Could be useful to run queries easier in the program
-'''
-A function to execute a query giving a query and set of parameters and return
-the results as a list of tuples.
-    query: a string which declares the query to be run
-    parameters: a dict which maps the variables declared in the query string
-    to the python variables intended
+    def loginUser():
+        exampleQuery = 'SELECT * FROM members WHERE email=:loginEmail AND passwd=:loginPasswd'
+        exampleParameters = {'loginEmail': emailVar, 'loginPasswd': passwdVar}
+        executeQuery(exampleQuery, exampleParameters)
 
-Example: simple login query
-exampleQuery = 'SELECT * FROM members WHERE email=:loginEmail AND passwd=:loginPasswd'
-exampleParameters = {'loginEmail': emailVar, 'loginPasswd': passwdVar}
-executeQuery(exampleQuery, exampleParameters)
-'''
-def executeQuery(query: str, parameters: dict) -> list:
-    c.execute(query, parameters)
-    rows = c.fetchall()
-    return rows
+    def registerUser():
+        pass
 
-def loginUser():
-    pass
-
-def registerUser():
-    pass
-
-
-
-response = ''
-validResponse = False
-while not validResponse:
-    response = input("Would you like to login or register? ")
-    if response.lower() == 'login' or response.lower() == 'register':
-        validResponse = True
-    else:
-        print("Invalid response, type either 'login' or 'register'")
-
-
-if response.lower() == 'login':
-    loginUser()
-elif response.lower() == 'register':
-    registerUser()
+    response = ''
+    validResponse = False
+    while not validResponse:
+        response = input("Would you like to login or register? ")
+        if response.lower() == 'login' or response.lower() == 'register':
+            validResponse = True
+        else:
+            print("Invalid response, type either 'login' or 'register'")
 
 
+    if response.lower() == 'login':
+        loginUser()
+    elif response.lower() == 'register':
+        registerUser()
 
-
-#conn.commit() # if you want to save changes to the db
-conn.close()	
+    #conn.commit() # if you want to save changes to the db
+    conn.close()
